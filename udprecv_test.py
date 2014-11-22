de30a23a1e2108c66cabc2a420ce46cf205dd210
João Taveira Araújo <joao.taveira@gmail.com>
2014-11-22 15:04:04 -0800

    Encode string in tests to ensure python3 compatibility

--- udprecv_test.py
+++ udprecv_test.py
@@ -14,6 +14,9 @@ class TestUdpRecv(unittest.TestCase):
         self.recv = udprecv.UdpRecv([1666, ])
         self.recv.start()
 
+        self.teststr = 'test'.encode('UTF-8')
+        self.testnum = '10'.encode('UTF-8')
+
     def tearDown(self):
         self.recv.stop()
 
@@ -35,19 +38,19 @@ class TestUdpRecv(unittest.TestCase):
         """ Send and receive single packet over IPv4. """
         assert not self.args
         self.recv.add_callback(self.register_cb)
-        self.sendto('test', ('127.0.0.1', 1666), socket.AF_INET)
-        assert self.args == [('127.0.0.1', 'test')]
+        self.sendto(self.teststr, ('127.0.0.1', 1666), socket.AF_INET)
+        assert self.args == [('127.0.0.1', self.teststr)]
 
     def test_recv_ipv6(self):
         """ Send and receive single packet over IPv4. """
         assert not self.args
         self.recv.add_callback(self.register_cb)
-        self.sendto('test', ('::1', 1666, 0, 0), socket.AF_INET6)
-        assert self.args == [('::1', 'test')]
+        self.sendto(self.teststr, ('::1', 1666, 0, 0), socket.AF_INET6)
+        assert self.args == [('::1', self.teststr)]
 
     def test_reader(self):
         self.recv.set_reader(int)
         self.recv.add_callback(self.register_cb)
-        self.sendto('10', ('::1', 1666, 0, 0), socket.AF_INET6)
-        assert self.args == [('::1', 10)]
+        self.sendto(self.testnum, ('::1', 1666, 0, 0), socket.AF_INET6)
+        assert self.args == [('::1', int(self.testnum))]
 
